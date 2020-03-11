d650337574137e0ecb95bb79df9ed87621148d2b
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-03-11 16:06:13 -0400

    Creates basic room class and adds the item file with basic item class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# Room Class
+# n_to s_to e_to and w_to are directional paths from current_room to the next room
+
+
+class Room:
+    def __init__(self, name, description, n_to, s_to, e_to, w_to):
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
